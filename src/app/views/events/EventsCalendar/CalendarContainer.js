30585955fba8da03d41c64d702cd21f9ff49b6a3
vahanmkrtumyan <35697975+vahanmkrtumyan@users.noreply.github.com>
2020-12-14 09:49:37 -0800

    Update CalendarContainer.js

--- src/app/views/events/EventsCalendar/CalendarContainer.js
+++ src/app/views/events/EventsCalendar/CalendarContainer.js
@@ -1,8 +1,8 @@
-import React, { useState, useCallback } from "react";
-import { Calendar, momentLocalizer } from "react-big-calendar";
-import moment from "moment";
-import "react-big-calendar/lib/css/react-big-calendar.css";
-import AddEvent from "./AddEvent";
+import React, { useState, useCallback } from 'react';
+import { Calendar, momentLocalizer, Views } from 'react-big-calendar';
+import moment from 'moment';
+import 'react-big-calendar/lib/css/react-big-calendar.css';
+import AddEvent from './AddEvent';
 
 const localizer = momentLocalizer(moment);
 
@@ -14,20 +14,36 @@ function CalendarContainer() {
   }, []);
 
   console.log(slot);
+
+  function handleSubmit(e) {
+    console.log(e);
+  }
+
   return (
     <div>
       <Calendar
         selectable={true}
         localizer={localizer}
+        timeslots={4}
+        defaultView={Views.WEEK}
         events={[]}
+        step={15}
         onSelectSlot={slotHandler}
-        startAccessor="start"
-        endAccessor="end"
+        startAccessor='start'
+        endAccessor='end'
         style={{ height: 500 }}
+        min={
+          new Date(
+            new Date().getFullYear(),
+            new Date().getMonth(),
+            new Date().getDate(),
+            8
+          )
+        }
       />
       {slot && (
         <div>
-          <AddEvent slotHandler={slotHandler} />
+          <AddEvent slotHandler={slotHandler} handleSubmit={handleSubmit} />
         </div>
       )}
     </div>
